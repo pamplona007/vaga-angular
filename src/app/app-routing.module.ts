098461a2a5d7abdb91c0d024e14763583481ab84
Lucas Pamplona <lucas250598@gmail.com>
2023-01-11 17:07:31 -0300

    Implemented layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImageComponent } from './pages/image/image.component';
+import { PanelComponent } from './pages/panel/panel.component';
+import { TableComponent } from './pages/table/table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: TableComponent,
+        title: 'Table',
+    },
+    {
+        path: 'image',
+        component: ImageComponent,
+        title: 'Image',
+    },
+    {
+        path: 'panel',
+        component: PanelComponent,
+        title: 'Panel',
+    },
+    {
+        path: '**',
+        redirectTo: '/table',
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
